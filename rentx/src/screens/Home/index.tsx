c0ec76da38d9c31d518e9203798ae4d47e2471c1
hedenica <hedenica@hotmail.com>
2021-09-26 20:34:06 -0300

    feat(rentx): initial commit

---
+++ rentx/src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+  Container,
+  Title,
+} from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Home Page</Title>
+    </Container>
+  );
+}
---
+++ rentx/src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  font-size: 30px;
+`;
