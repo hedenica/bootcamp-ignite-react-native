97464f7f84fae886ba32ec57d0eca18e2eecf402
hedenica <hedenica@hotmail.com>
2021-09-27 10:32:43 -0300

    feat(header): add header

--- rentx/src/screens/Home/index.tsx
+++ rentx/src/screens/Home/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize'
+
+import Logo from '../../assets/logo.svg'
 
 import {
   Container,
-  Title,
+  Header,
+  HeaderContent,
+  TotalCars,
 } from './styles';
 
 export function Home() {
   return (
     <Container>
-      <Title>Home Page</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+
+      </Header>
     </Container>
   );
 }
--- rentx/src/screens/Home/styles.ts
+++ rentx/src/screens/Home/styles.ts
@@ -1,14 +1,29 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.secondary_600};
-  font-size: 30px;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
