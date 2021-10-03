66848ff01d5e86f76fe7e067f8695f37797e9102
hedenica <hedenica@hotmail.com>
2021-10-03 09:57:44 -0300

    feat(scheduling-details): add scheduling details screen

---
+++ rentx/src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import { useTheme } from 'styled-components';
+import { useNavigation } from '@react-navigation/native'
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateValue,
+  DateTitle,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+  Footer,
+} from './styles';
+
+type NavigationProps = {
+  navigate:(screen:string) => void;
+  goBack: () => void;
+}
+
+export function SchedulingDetails() {
+  const { navigate, goBack } = useNavigation<NavigationProps>();
+  const theme = useTheme();
+
+
+  function handleConfirmRental() {
+    navigate('SchedulingComplete');
+  }
+
+  return (
+    <Container>
+      <StatusBar barStyle="dark-content" />
+      <Header>
+        <BackButton onPress={() => goBack()} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider imagesUrl={['https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather name="calendar" size={RFValue(24)} color={theme.colors.shape} />
+          </CalendarIcon>
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+
+          <Feather name="chevron-right" size={RFValue(12)} color={theme.colors.shape} />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+
+      <Footer>
+        <Button title="Alugar agora" color={theme.colors.success} onPress={handleConfirmRental} />
+      </Footer>
+
+    </Container>
+  );
+}
