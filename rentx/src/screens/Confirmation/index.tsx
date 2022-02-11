5265e8f3cc0ae9933eda6f2c4777b8df994c7c35
hedenica <hedenica@hotmail.com>
2022-02-11 09:27:48 -0300

    refact: make screen reusable

--- rentx/src/screens/Confirmation/index.tsx
+++ rentx/src/screens/Confirmation/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar, useWindowDimensions } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
 import LogoSvg from '../../assets/logo_background_gray.svg';
 import DoneSvg from '../../assets/done.svg';
@@ -19,14 +19,21 @@ type NavigationProps = {
   navigate:(screen:string) => void;
 }
 
-export function SchedulingComplete() {
+type Params = {
+  title: string
+  message: string
+  nextScreen: string
+}
+
+export function Confirmation() {
   const { width } = useWindowDimensions()
   const { navigate } = useNavigation<NavigationProps>();
+  const { params } = useRoute()
+  const { title, message, nextScreen } = params as Params
 
   function handleReturnHome() {
-    navigate('Home');
+    navigate(nextScreen);
   }
-
 
   return (
     <Container>
@@ -35,12 +42,8 @@ export function SchedulingComplete() {
 
       <Content>
         <DoneSvg width={80} height={80} />
-        <Title>Carro alugado!</Title>
-        <Message> 
-          Agora você só precisa ir {'\n'}
-          até a concessionária da RENTX {'\n'}
-          pegar seu automóvel.
-        </Message>
+        <Title>{title}</Title>
+        <Message>{message}</Message>
       </Content>
       <Footer>
         <ConfirmButton title="ok" onPress={handleReturnHome} />
